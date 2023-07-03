fb703a023f6df306797db61e3188d607a3f78ca9
unknown <dasilvadeoliveirafabio@gmail.com>
2023-07-03 13:21:45 -0300

    alteração de expiração do jwt

--- src/controllers/autenticacaoController.ts
+++ src/controllers/autenticacaoController.ts
@@ -47,7 +47,7 @@ export const  logar = async(req:Request, res:Response)=>{
           }
        }
       })
-      const token = jwt.sign({id:auth?.id},secret,{expiresIn:'1d'})
+      const token = jwt.sign({id:auth?.id},secret,{expiresIn:"30s"})
       if (auth) {
           res.json({auth, token})
       }else{
