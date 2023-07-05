33308c9c82690a4aa702e20afa32848c24f1bacc
unknown <dasilvadeoliveirafabio@gmail.com>
2023-07-04 23:48:50 -0300

    alteração no endpoint de jogador

--- src/metodosUteis.ts
+++ src/metodosUteis.ts
@@ -35,61 +35,60 @@ export async function transferenciaMonetaria(idDoProprietario:string, idDoCompra
             }
         })
         return true
-    }
-    // else{
-    //     const saldoDoProprietario = await prisma.participantes.findUnique({
-    //         where:{
-    //             id:idDoProprietario
-    //         },
-    //         select:{
-    //             saldo:true
-    //         }
-    //     })
-    //     const saldoDoComprador = await prisma.participantes.findUnique({
-    //         where:{
-    //             id:idDoComprador
-    //         },
-    //         select:{
-    //             saldo:true
-    //         }
-    //     }) 
+    }else{
+        const saldoDoProprietario = await prisma.participantes.findUnique({
+            where:{
+                id:idDoProprietario
+            },
+            select:{
+                saldo:true
+            }
+        })
+        const saldoDoComprador = await prisma.participantes.findUnique({
+            where:{
+                id:idDoComprador
+            },
+            select:{
+                saldo:true
+            }
+        }) 
         
-    //     const arrayValores = await prisma.jogadores.findMany({
-    //         where:{
-    //             id:{
-    //                 in:idsDosJogadoresSelecionados
-    //             }
-    //         },
-    //         select:{
-    //             valorDoJogador:true
-    //         } 
-    //     });
-    //     const somaDosValoresDosJogadores = arrayValores.reduce((acc:any, v:any)=>{
-    //        return acc + parseFloat(v.valorDoJogador)
-    //     },0)
+        const arrayValores = await prisma.jogadores.findMany({
+            where:{
+                id:{
+                    in:idsDosJogadoresSelecionados
+                }
+            },
+            select:{
+                valorDoJogador:true
+            } 
+        });
+        const somaDosValoresDosJogadores = arrayValores.reduce((acc:any, v:any)=>{
+           return acc + parseFloat(v.valorDoJogador)
+        },0)
     
-    //     if (somaDosValoresDosJogadores > (saldoDoComprador?.saldo || 0)) {
-    //         return false
-    //     }else{
-    //         await prisma.participantes.update({
-    //             where:{
-    //                 id:idDoComprador
-    //             },
-    //             data:{
-    //                 saldo: (saldoDoComprador?.saldo || 0) - somaDosValoresDosJogadores
-    //             }
-    //         })
-    //         await prisma.participantes.update({
-    //             where:{
-    //                 id:idDoProprietario
-    //             },
-    //             data:{
-    //                 saldo: (saldoDoProprietario?.saldo || 0) + somaDosValoresDosJogadores
-    //             }
-    //         })
-    //         return true
-    //     }
-    // }
+        if (somaDosValoresDosJogadores > (saldoDoComprador?.saldo || 0)) {
+            return false
+        }else{
+            await prisma.participantes.update({
+                where:{
+                    id:idDoComprador
+                },
+                data:{
+                    saldo: (saldoDoComprador?.saldo || 0) - somaDosValoresDosJogadores
+                }
+            })
+            await prisma.participantes.update({
+                where:{
+                    id:idDoProprietario
+                },
+                data:{
+                    saldo: (saldoDoProprietario?.saldo || 0) + somaDosValoresDosJogadores
+                }
+            })
+            return true
+        }
+    }
 }
 
 export const tratarValorDoJogador = (valor:string | undefined)=>{
