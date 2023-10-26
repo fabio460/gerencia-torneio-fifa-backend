4dd3831b6bd524dd06451e115d4f8a28884f9bd0
fabio460 <dasilvadeoliveirafabio@gmail.com>
2023-10-26 00:14:43 -0300

    inserção de uma nova tabela de resultados

--- src/controllers/tabeladeResultadosController.ts
+++ src/controllers/tabeladeResultadosController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express"
 import { resultadoType } from "../types"
 const prisma = new PrismaClient()
 
+
 export const criar = async(req:Request, res: Response)=>{
    const {tabelaDePremiados} = req.body 
    const tabela:resultadoType[] = tabelaDePremiados
